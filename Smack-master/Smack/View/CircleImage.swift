42c5c62f78d93c9e28912b65fc50b876e7e69278
Jefffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-15 23:32:01 -0500

    Completed color generator, made profile images round, changed the color of placeholder text

---
+++ Smack-master/Smack/View/CircleImage.swift
@@ -0,0 +1,29 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Jefffrey Santana on 11/15/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setUpView()
+    }
+    
+}
