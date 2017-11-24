d4dbc1be518c579472fb43b9050318f1af0a274a
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-23 22:54:38 -0500

    Created new service for messages; able to retrieve all messages made

--- Smack-master/Smack/Controller/ChatVC.swift
+++ Smack-master/Smack/Controller/ChatVC.swift
@@ -50,13 +50,26 @@ class ChatVC: UIViewController {
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLoginGetMessages() {
         MessageService.instance.getAllChannels { (success) in
             if success {
-                //do stuff with channels
+                if MessageService.instance.channels.count > 0 {
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                } else {
+                    self.channelNameLbl.text = "No channels yet!"
+                }
             }
+        }
+    }
+    
+    func getMessages() {
+        guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+        MessageService.instance.getAllMessagesForChannel(channelId: channelId) { (success) in
+            
         }
     }
 
---
+++ Smack-master/Smack/Services/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Jeffrey Santana on 11/23/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var Id: String!
+    public private(set) var timestamp: String!
+    
+}
