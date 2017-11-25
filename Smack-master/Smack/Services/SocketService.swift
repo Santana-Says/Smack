ecaeffd4f9d7fc0d88cec4f7a3e6f79f4d64c369
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-11-25 16:27:00 -0500

    Added feature to indicate which channel has an unread message

--- Smack-master/Smack/Services/SocketService.swift
+++ Smack-master/Smack/Services/SocketService.swift
@@ -50,7 +50,7 @@ class SocketService: NSObject {
         completion(true)
     }
     
-    func getChatMessage(completion: @escaping CompletionHandler) {
+    func getChatMessage(completion: @escaping (_ newMesssage: Message) -> Void) {
         socket.on("messageCreated") { (dataArray, ack) in
             guard let messageBody = dataArray[0] as? String else { return }
             guard let channelId = dataArray[2] as? String else { return }
@@ -60,14 +60,9 @@ class SocketService: NSObject {
             guard let id = dataArray[6] as? String else { return }
             guard let timestamp = dataArray[7] as? String else { return }
             
-            //Check if incoming message is part of selectedChannel
-            if channelId == MessageService.instance.selectedChannel?.id && AuthService.instance.isLoggedIn {
-                let newMessage = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, Id: id, timestamp: timestamp)
-                MessageService.instance.messages.append(newMessage)
-                completion(true)
-            } else {
-                completion(false)
-            }
+            let newMessage = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, Id: id, timestamp: timestamp)
+            
+            completion(newMessage)
         }
     }
     
